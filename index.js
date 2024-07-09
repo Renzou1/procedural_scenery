4ba39e0f996b6483af524d662a6b473acf51d926
Renzo Battisti <renzo.battisti1@gmail.com>
2024-07-08 21:29:09 -0300

    moving camera instead of world

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-// WebGL2 - load obj - cube
-// from https://webgl2fundamentals.org/webgl/webgl-load-obj-cube.html
-
-
 "use strict";
+
+// This is not a full .obj parser.
+// see http://paulbourke.net/dataformats/obj/
 
 function parseOBJ(text) {
   // because indices are base 1 let's just fill in the 0th data
   const objPositions = [[0, 0, 0]];
   const objTexcoords = [[0, 0]];
   const objNormals = [[0, 0, 0]];
+  const objColors = [[0, 0, 0]];
 
   // same order as `f` indices
   const objVertexData = [
     objPositions,
     objTexcoords,
     objNormals,
+    objColors,
   ];
 
   // same order as `f` indices
@@ -22,7 +23,17 @@ function parseOBJ(text) {
     [],   // positions
     [],   // texcoords
     [],   // normals
+    [],   // colors
   ];
+
+  const materialLibs = [];
+  const geometries = [];
+  let geometry;
+  let groups = ['default'];
+  let material = 'default';
+  let object = 'default';
+
+  const noop = () => {};
 
   function newGeometry() {
     // If there is an existing geometry and it's
@@ -30,7 +41,33 @@ function parseOBJ(text) {
     if (geometry && geometry.data.position.length) {
       geometry = undefined;
     }
-    setGeometry();
+  }
+
+  function setGeometry() {
+    if (!geometry) {
+      const position = [];
+      const texcoord = [];
+      const normal = [];
+      const color = [];
+      webglVertexData = [
+        position,
+        texcoord,
+        normal,
+        color,
+      ];
+      geometry = {
+        object,
+        groups,
+        material,
+        data: {
+          position,
+          texcoord,
+          normal,
+          color,
+        },
+      };
+      geometries.push(geometry);
+    }
   }
 
   function addVertex(vert) {
@@ -42,12 +79,23 @@ function parseOBJ(text) {
       const objIndex = parseInt(objIndexStr);
       const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
       webglVertexData[i].push(...objVertexData[i][index]);
+      // if this is the position index (index 0) and we parsed
+      // vertex colors then copy the vertex colors to the webgl vertex color data
+      if (i === 0 && objColors.length > 1) {
+        geometry.data.color.push(...objColors[index]);
+      }
     });
   }
 
   const keywords = {
     v(parts) {
-      objPositions.push(parts.map(parseFloat));
+      // if there are more than 3 values here they are vertex colors
+      if (parts.length > 3) {
+        objPositions.push(parts.slice(0, 3).map(parseFloat));
+        objColors.push(parts.slice(3).map(parseFloat));
+      } else {
+        objPositions.push(parts.map(parseFloat));
+      }
     },
     vn(parts) {
       objNormals.push(parts.map(parseFloat));
@@ -57,12 +105,31 @@ function parseOBJ(text) {
       objTexcoords.push(parts.map(parseFloat));
     },
     f(parts) {
+      setGeometry();
       const numTriangles = parts.length - 2;
       for (let tri = 0; tri < numTriangles; ++tri) {
         addVertex(parts[0]);
         addVertex(parts[tri + 1]);
         addVertex(parts[tri + 2]);
       }
+    },
+    s: noop,    // smoothing group
+    mtllib(parts, unparsedArgs) {
+      // the spec says there can be multiple filenames here
+      // but many exist with spaces in a single filename
+      materialLibs.push(unparsedArgs);
+    },
+    usemtl(parts, unparsedArgs) {
+      material = unparsedArgs;
+      newGeometry();
+    },
+    g(parts) {
+      groups = parts;
+      newGeometry();
+    },
+    o(parts, unparsedArgs) {
+      object = unparsedArgs;
+      newGeometry();
     },
   };
 
@@ -87,11 +154,68 @@ function parseOBJ(text) {
     handler(parts, unparsedArgs);
   }
 
+  // remove any arrays that have no entries.
+  for (const geometry of geometries) {
+    geometry.data = Object.fromEntries(
+        Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+  }
+
   return {
-    position: webglVertexData[0],
-    texcoord: webglVertexData[1],
-    normal: webglVertexData[2],
+    geometries,
+    materialLibs,
   };
+}
+
+function parseMapArgs(unparsedArgs) {
+  // TODO: handle options
+  return unparsedArgs;
+}
+
+function parseMTL(text) {
+  const materials = {};
+  let material;
+
+  const keywords = {
+    newmtl(parts, unparsedArgs) {
+      material = {};
+      materials[unparsedArgs] = material;
+    },
+    /* eslint brace-style:0 */
+    Ns(parts)       { material.shininess      = parseFloat(parts[0]); },
+    Ka(parts)       { material.ambient        = parts.map(parseFloat); },
+    Kd(parts)       { material.diffuse        = parts.map(parseFloat); },
+    Ks(parts)       { material.specular       = parts.map(parseFloat); },
+    Ke(parts)       { material.emissive       = parts.map(parseFloat); },
+    map_Kd(parts, unparsedArgs)   { material.diffuseMap = parseMapArgs(unparsedArgs); },
+    map_Ns(parts, unparsedArgs)   { material.specularMap = parseMapArgs(unparsedArgs); },
+    map_Bump(parts, unparsedArgs) { material.normalMap = parseMapArgs(unparsedArgs); },
+    Ni(parts)       { material.opticalDensity = parseFloat(parts[0]); },
+    d(parts)        { material.opacity        = parseFloat(parts[0]); },
+    illum(parts)    { material.illum          = parseInt(parts[0]); },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  return materials;
 }
 
 async function main() {
@@ -109,16 +233,26 @@ async function main() {
   const vs = `#version 300 es
   in vec4 a_position;
   in vec3 a_normal;
+  in vec2 a_texcoord;
+  in vec4 a_color;
 
   uniform mat4 u_projection;
   uniform mat4 u_view;
   uniform mat4 u_world;
+  uniform vec3 u_viewWorldPosition;
 
   out vec3 v_normal;
+  out vec3 v_surfaceToView;
+  out vec2 v_texcoord;
+  out vec4 v_color;
 
   void main() {
-    gl_Position = u_projection * u_view * u_world * a_position;
+    vec4 worldPosition = u_world * a_position;
+    gl_Position = u_projection * u_view * worldPosition;
+    v_surfaceToView = u_viewWorldPosition - worldPosition.xyz;
     v_normal = mat3(u_world) * a_normal;
+    v_texcoord = a_texcoord;
+    v_color = a_color;
   }
   `;
 
@@ -126,16 +260,41 @@ async function main() {
   precision highp float;
 
   in vec3 v_normal;
+  in vec3 v_surfaceToView;
+  in vec2 v_texcoord;
+  in vec4 v_color;
 
-  uniform vec4 u_diffuse;
+  uniform vec3 diffuse;
+  uniform sampler2D diffuseMap;
+  uniform vec3 ambient;
+  uniform vec3 emissive;
+  uniform vec3 specular;
+  uniform float shininess;
+  uniform float opacity;
   uniform vec3 u_lightDirection;
+  uniform vec3 u_ambientLight;
 
   out vec4 outColor;
 
   void main () {
     vec3 normal = normalize(v_normal);
+
+    vec3 surfaceToViewDirection = normalize(v_surfaceToView);
+    vec3 halfVector = normalize(u_lightDirection + surfaceToViewDirection);
+
     float fakeLight = dot(u_lightDirection, normal) * .5 + .5;
-    outColor = vec4(u_diffuse.rgb * fakeLight, u_diffuse.a);
+    float specularLight = clamp(dot(normal, halfVector), 0.0, 1.0);
+
+    vec4 diffuseMapColor = texture(diffuseMap, v_texcoord);
+    vec3 effectiveDiffuse = diffuse * diffuseMapColor.rgb * v_color.rgb;
+    float effectiveOpacity = opacity * diffuseMapColor.a * v_color.a;
+
+    outColor = vec4(
+        emissive +
+        ambient * u_ambientLight +
+        effectiveDiffuse * fakeLight +
+        specular * pow(specularLight, shininess),
+        effectiveOpacity);
   }
   `;
 
@@ -143,39 +302,131 @@ async function main() {
   // compiles and links the shaders, looks up attribute and uniform locations
   const meshProgramInfo = twgl.createProgramInfo(gl, [vs, fs]);
 
-  const response = await fetch('hex_grass_bottom.obj');
+  const objHref = 'hex_water.obj';  
+  const response = await fetch(objHref);
   const text = await response.text();
-  const data = parseOBJ(text);
+  const obj = parseOBJ(text);
+  const baseHref = new URL(objHref, window.location.href);
+  const matTexts = await Promise.all(obj.materialLibs.map(async filename => {
+    const matHref = new URL(filename, baseHref).href;
+    const response = await fetch(matHref);
+    return await response.text();
+  }));
+  const materials = parseMTL(matTexts.join('\n'));
 
-  // Because data is just named arrays like this
-  //
-  // {
-  //   position: [...],
-  //   texcoord: [...],
-  //   normal: [...],
-  // }
-  //
-  // and because those names match the attributes in our vertex
-  // shader we can pass it directly into `createBufferInfoFromArrays`
-  // from the article "less code more fun".
+  const textures = {
+    defaultWhite: twgl.createTexture(gl, {src: [255, 255, 255, 255]}),
+  };
 
-  // create a buffer for each array by calling
-  // gl.createBuffer, gl.bindBuffer, gl.bufferData
-  const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
-  // fills out a vertex array by calling gl.createVertexArray, gl.bindVertexArray
-  // then gl.bindBuffer, gl.enableVertexAttribArray, and gl.vertexAttribPointer for each attribute
-  const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+  // load texture for materials
+  for (const material of Object.values(materials)) {
+    Object.entries(material)
+      .filter(([key]) => key.endsWith('Map'))
+      .forEach(([key, filename]) => {
+        let texture = textures[filename];
+        if (!texture) {
+          const textureHref = new URL(filename, baseHref).href;
+          texture = twgl.createTexture(gl, {src: textureHref, flipY: true});
+          textures[filename] = texture;
+        }
+        material[key] = texture;
+      });
+  }
 
-  const response2 = await fetch('building_mine_blue.obj');
-  const text2 = await response2.text();
-  const data2 = parseOBJ(text2);
-  const bufferInfo2 = twgl.createBufferInfoFromArrays(gl, data2);
-  const vao2 = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo2);
+  const defaultMaterial = {
+    diffuse: [1, 1, 1],
+    diffuseMap: textures.defaultWhite,
+    ambient: [0, 0, 0],
+    specular: [1, 1, 1],
+    shininess: 400,
+    opacity: 1,
+  };
 
+  const parts = obj.geometries.map(({material, data}) => {
+    // Because data is just named arrays like this
+    //
+    // {
+    //   position: [...],
+    //   texcoord: [...],
+    //   normal: [...],
+    // }
+    //
+    // and because those names match the attributes in our vertex
+    // shader we can pass it directly into `createBufferInfoFromArrays`
+    // from the article "less code more fun".
+
+    if (data.color) {
+      if (data.position.length === data.color.length) {
+        // it's 3. The our helper library assumes 4 so we need
+        // to tell it there are only 3.
+        data.color = { numComponents: 3, data: data.color };
+      }
+    } else {
+      // there are no vertex colors so just use constant white
+      data.color = { value: [1, 1, 1, 1] };
+    }
+
+    // create a buffer for each array by calling
+    // gl.createBuffer, gl.bindBuffer, gl.bufferData
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, data);
+    const vao = twgl.createVAOFromBufferInfo(gl, meshProgramInfo, bufferInfo);
+    return {
+      material: {
+        ...defaultMaterial,
+        ...materials[material],
+      },
+      bufferInfo,
+      vao,
+    };
+  });
+
+  function getExtents(positions) {
+    const min = positions.slice(0, 3);
+    const max = positions.slice(0, 3);
+    for (let i = 3; i < positions.length; i += 3) {
+      for (let j = 0; j < 3; ++j) {
+        const v = positions[i + j];
+        min[j] = Math.min(v, min[j]);
+        max[j] = Math.max(v, max[j]);
+      }
+    }
+    return {min, max};
+  }
+
+  function getGeometriesExtents(geometries) {
+    return geometries.reduce(({min, max}, {data}) => {
+      const minMax = getExtents(data.position);
+      return {
+        min: min.map((min, ndx) => Math.min(minMax.min[ndx], min)),
+        max: max.map((max, ndx) => Math.max(minMax.max[ndx], max)),
+      };
+    }, {
+      min: Array(3).fill(Number.POSITIVE_INFINITY),
+      max: Array(3).fill(Number.NEGATIVE_INFINITY),
+    });
+  }
+
+  const extents = getGeometriesExtents(obj.geometries);
+  const range = m4.subtractVectors(extents.max, extents.min);
+  // amount to move the object so its center is at the origin
+  const objOffset = m4.scaleVector(
+      m4.addVectors(
+        extents.min,
+        m4.scaleVector(range, 0.5)),
+      -1);
   const cameraTarget = [0, 0, 0];
-  const cameraPosition = [0, 2, 3];
-  const zNear = 0.1;
-  const zFar = 50;
+  // figure out how far away to move the camera so we can likely
+  // see the object.
+  const radius = m4.length(range) * 1.2;
+  let cameraPosition = m4.addVectors(cameraTarget, [
+    0,
+    2,
+    radius,
+  ]);
+  // Set zNear and zFar to something hopefully appropriate
+  // for the size of this object.
+  const zNear = radius / 100;
+  const zFar = radius * 3;
 
   function degToRad(deg) {
     return deg * Math.PI / 180;
@@ -187,46 +438,51 @@ async function main() {
     twgl.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
 
     const fieldOfViewRadians = degToRad(60);
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
     const up = [0, 1, 0];
-    // Compute the camera's matrix using look at.
-    const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+
+    //const camera = m4.lookAt(cameraPosition, cameraTarget, up);
+
+    let camera = m4.yRotation(1 * time);
+    camera = m4.translate(camera, 0, 0, radius * 1.5);
 
     // Make a view matrix from the camera matrix.
     const view = m4.inverse(camera);
+
+    //var viewProjectionMatrix = m4.multiplty(projection, view);
 
     const sharedUniforms = {
       u_lightDirection: m4.normalize([-1, 3, 5]),
       u_view: view,
       u_projection: projection,
+      u_viewWorldPosition: cameraPosition,
     };
 
     gl.useProgram(meshProgramInfo.program);
 
     // calls gl.uniform
     twgl.setUniforms(meshProgramInfo, sharedUniforms);
-    
-    // calls gl.uniform
-    twgl.setUniforms(meshProgramInfo, {
-        u_world: m4.yRotation(time * 0.2),
-        u_diffuse: [1, 0.7, 0.5, 1],
-    });
 
-    // set the attributes for this part.
-    gl.bindVertexArray(vao);
+    // compute the world matrix once since all parts
+    // are at the same space.
+    let u_world = m4.yRotation(0)
+    //let u_world = m4.yRotation(time * 0.25); // hacky? camera should move
+    //u_world = m4.translate(u_world, ...objOffset);
 
-    // calls gl.drawArrays or gl.drawElements
-    twgl.drawBufferInfo(gl, bufferInfo);
-
-    gl.bindVertexArray(vao2);
-
-    // calls gl.drawArrays or gl.drawElements
-    twgl.drawBufferInfo(gl, bufferInfo2);
+    for (const {bufferInfo, vao, material} of parts) {
+      // set the attributes for this part.
+      gl.bindVertexArray(vao);
+      // calls gl.uniform
+      twgl.setUniforms(meshProgramInfo, {
+        u_world,
+      }, material);
+      // calls gl.drawArrays or gl.drawElements
+      twgl.drawBufferInfo(gl, bufferInfo);
+    }
 
     requestAnimationFrame(render);
   }
